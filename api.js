956f826a20723c52f443394e51d1cf71b5296a7c
Navi sureka <navisureka23@gmail.com>
2022-04-10 18:46:37 +0530

    for heroku

--- api.js
+++ api.js
@@ -1,99 +1,21 @@
 const path = require('path')
 const express = require('express')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const fs = require('fs');
 
 const app = express()
 
 const port =process.env.PORT || 3000
 
-// Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+let rawdata = fs.readFileSync('newsData.json');
+let news= JSON.parse(rawdata);
 
-// Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
 
-// Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
-
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Navi Sureka'
-    })
-})
-
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Navi Sureka'
-    })
-})
-
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'Hello! If you find any problem using this application then feel free to contact me!',
-        title: 'Help',
-        name: 'Navi Sureka'
-    })
-})
-
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
-    geocode(req.query.address, (error, {name,lat,long } = {}) => {
-        if (error) {
-            return res.send({ error })
-        }
-        forecast(lat, long, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-
-            res.send({
-                forecast: forecastData,
-                name,
-                address: req.query.address
-            })
-        })
-    })
-})
-
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
-    }
-
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
-
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Navi Sureka',
-        errorMessage: 'Help article not found.'
-    })
+app.get('/news', (req, res) => {
+    res.send(news[Math.floor((Math.random() * 99))])
 })
 
 app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Navi Sureka',
-        errorMessage: 'Page not found.'
-    })
+    res.send("Goto /news to fetch random news")
 })
 
 app.listen(port, () => {
